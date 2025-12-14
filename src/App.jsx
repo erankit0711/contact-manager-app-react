371f8c85e396bc4d2dd501df1b2b90f04cc2a685
Ankit kumar Gupta <erankit0711@gmail.com>
2025-12-14 08:07:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+export default App
+
+
+
+/*Components
+-> ContactManagerPage 
+    -> Header
+    -> Title
+    -> Container
+        -> Search
+        -> AddContact
+        -> Table
+            -> Edit
+            -> Delete
+*/
