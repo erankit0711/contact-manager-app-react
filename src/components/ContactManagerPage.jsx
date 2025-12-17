8ed9c8ed6b05304e0e1876de2bfb8a8defb3197f
Ankit kumar Gupta <erankit0711@gmail.com>
2025-12-18 00:05:00 +0530

    css refactoring

--- src/components/ContactManagerPage.jsx
+++ src/components/ContactManagerPage.jsx
@@ -13,18 +13,32 @@ function ContactManagerPage(){
 
     return (
         <div className="contact-page">
+
             <div className="header">
                 <img src={logo} alt="ofbusiness-logo" className="header-logo" />
             </div>
-            <div className="container">
-                <h2>Contact Manager</h2>
-                <div className="container-bottom">
-                    <Search setSearchTerm={setSearchTerm}/>
-                    <AddContactButton isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
-                    <TableContainer searchTerm={searchTerm}/>
+
+            <div className="page-content">
+
+                <div className="page-header">
+                    <h2>Contact Manager</h2>
                 </div>
+
+                <div className="page-container">
+                    
+                    <div className="container-top">
+                        <Search setSearchTerm={setSearchTerm}/>
+                        <AddContactButton isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+                    </div>
+
+                    <div className="container-bottom">
+                        <TableContainer searchTerm={searchTerm}/>
+                    </div>
+                </div>
+                    
                 {isModalOpen && <AddContactForm setIsModalOpen={setIsModalOpen}/>}
             </div>
+
         </div>
     )
 }
